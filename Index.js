3b42d40e141bb8ef81a147bbe571dc85be0827e5
Ryusen9 <asratul02@gmail.com>
2025-03-29 18:53:20 +0600

    code review

--- Index.js
+++ Index.js
@@ -17,6 +17,8 @@ app.use(
       "https://marathonmate-76eca.firebaseapp.com/",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
 app.use(express.json());
